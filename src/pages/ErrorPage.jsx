42df599083e2be7ac585dd13b5628da07a27826c
hannahnier <hannahr@posteo.de>
2024-06-08 12:26:55 +0200

    style ErrorPage for darkmode/lightmode

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,33 +1,13 @@
+import style from "../styles/error.module.css";
+
 const ErrorPage = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        flexWrap: "wrap",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "80vh",
-      }}
-    >
-      <h1 style={{ margin: "0.5em" }}>404</h1>
-      <h2 style={{ margin: "0.5em" }}>Error</h2>
+    <div className={style.main}>
+      <h1 className={style.title}>404</h1>
+      <h2 className={style.title}>Error</h2>
       <p>The page you were looking for could not be found.</p>
 
-      <a
-        href="/"
-        style={{
-          background: "rgb(247, 240, 159)",
-          border: "none",
-          borderRadius: "0.5em",
-          padding: "0.5em",
-          margin: "50px",
-          color: "rgb(8, 10, 37)",
-          textDecoration: "none",
-        }}
-        // onClick={() => setActiveNav("#")}
-        // className={activeNav === "#" ? style.active : ""}
-      >
+      <a href="/" className={style.link}>
         Back to Start
       </a>
     </div>

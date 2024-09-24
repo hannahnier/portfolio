4720dae385ad6b87d6597f82c884ae8e91e27391
hannahnier <hannahr@posteo.de>
2024-09-24 18:40:15 +0200

    feature/keepAlive: new route and controller for regular ping cron jobs to keep the server running

--- backend/controller.js
+++ backend/controller.js
@@ -1,5 +1,13 @@
 import { sendEmail } from "./sendEmail.js";
 
+export const keepAlive = async (req, res, next) => {
+  try {
+    res.status(200).json({ msg: "server is running" });
+  } catch (e) {
+    next(e);
+  }
+};
+
 export const sendForm = async (req, res, next) => {
   const { contactName, contactEmail, contactMessage } = req.body;
   try {

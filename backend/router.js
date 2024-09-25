cc0a5df7804ac701a2952f004d4e1a67a286a7fd
hannahnier <hannahr@posteo.de>
2024-09-25 11:51:50 +0200

    fix/ping: fix controller for keeping server alive via cron job

--- backend/router.js
+++ backend/router.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { sendForm } from "./controller.js";
-import { keepAlive } from "./controller.js";
+import { getUser } from "./controller.js";
 
 export const router = express.Router();
 
-router.route("/").get(keepAlive);
+router.route("/").get(getUser);
 router.route("/").post(sendForm);
